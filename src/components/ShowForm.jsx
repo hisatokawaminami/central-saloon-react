6ee637dbdc2e4d4b5e47ae5bb88a62d72a228a00
Guest User <Guest@epicodus-41.local>
2018-09-07 14:47:25 -0700

    add showform component

---
+++ src/components/ShowForm.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+function ShowForm() {
+  return (
+    <div>
+      <form>
+        <input
+          type='text'
+          id='date'
+          placeholder='Date'
+        />
+        <input
+          type='text'
+          id='headliner'
+          placeholder='Headliner'
+        />
+        <input
+          type='text'
+          id='opener'
+          placeholder='Opener'
+        />
+        <input
+          type='text'
+          id='time'
+          placeholder='Door Time'
+        />
+        <input
+          type='text'
+          id='price'
+          placeholder='Ticket Price'
+        />
+      </form>
+    </div>
+  );
+}
